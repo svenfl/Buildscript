88f233f326f3104e226e868600f59bc051b8be42
svenfl <sventhomsen@foni.net>
2016-02-16 00:53:00 +0100

    added new targets
    added the targets  brcm2708-bcm2708 brcm2708-bcm2709 ramips-rt305x and sunxi

--- makeallbs.sh
+++ makeallbs.sh
@@ -2,7 +2,7 @@
 OPTIONS="DEFAULT_GLUON_BRANCH=stable DEFAULT_GLUON_RELEASE=stable-2016.1.0-0"
 #OPTIONS="BROKEN=1 DEFAULT_GLUON_RELEASE=2016.1-exp-$(date  '+%m-%d_%H_%M')"
 CORES=$(lscpu|grep -e '^CPU(s):'|xargs|cut -d" " -f2)
-for TARGET in ar71xx-generic ar71xx-nand mpc85xx-generic x86-generic x86-kvm_guest x86-64 x86-xen_domu; do
+for TARGET in ar71xx-generic ar71xx-nand brcm2708-bcm2708 brcm2708-bcm2709 mpc85xx-generic ramips-rt305x sunxi x86-64  x86-generic x86-kvm_guest x86-xen_domu; do
   echo "################# $TARGET ###########################"
   make -j$CORES GLUON_TARGET=$TARGET $OPTIONS || break 
 done && echo "alle Targets wurden erfolgreich erstellt." || echo "Fehlschlag! Abbruch"
